b72c9ef0f75a42bab75155c90548615418b291dd
Stan <kulyavtsev@yahoo.com>
2015-10-17 21:04:28 -0700

    init commit

---
+++ src/app/index.constants.js
@@ -0,0 +1,11 @@
+/* global malarkey:false, toastr:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('travelNg')
+    .constant('malarkey', malarkey)
+    .constant('toastr', toastr)
+    .constant('moment', moment);
+
+})();
---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('travelNg', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ngRoute']);
+
+})();
