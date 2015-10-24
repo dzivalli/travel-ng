89fc126c8978d8500e94125f749d3186ff79728e
Stan <kulyavtsev@yahoo.com>
2015-10-24 14:04:13 -0700

    add select by place

--- src/app/tours/tours.controller.js
+++ src/app/tours/tours.controller.js
@@ -23,33 +23,39 @@
       {query: { isArray: true, transformResponse: parseResult }}
     );
 
+    var Place = $resource(
+      'https://api.parse.com/1/classes/Place/:objectId',
+      { objectId: '@objectId' },
+      {query: { isArray: true, transformResponse: parseResult }}
+    );
+
     $scope.tours = Tour.query(function(data){
       $scope.selectedTours = data;
     });
 
     $scope.countries = Country.query();
+    $scope.places = Place.query();
 
-    $scope.countryByObjectId = function(objectId) {
-      var country = {name: ''};
+    $scope.objectByObjectId = function(objectsList, objectId) {
+      var obj = {};
 
-      angular.forEach($scope.countries, function(val) {
+      angular.forEach(objectsList, function(val) {
         if (val.objectId === objectId) {
-          country =  val;
+          obj = val;
         }
       });
-
-      return country;
+      return obj;
     };
 
-    $scope.toursByCountry = function() {
+    $scope.toursBy = function(objectName) {
       var tours = [];
 
-      if ($scope.chosenCountry === '') {
+      if ($scope.chosenItem === '') {
         $scope.selectedTours = $scope.tours;
       } else {
-        angular.forEach($scope.tours, function(v) {
-          if (v.country.objectId === $scope.chosenCountry) {
-            tours.push(v);
+        angular.forEach($scope.tours, function(val) {
+          if (val[objectName] && (val[objectName].objectId === $scope.chosenItem)) {
+            tours.push(val);
           }
         });
 
