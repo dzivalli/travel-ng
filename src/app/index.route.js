1582d25fee2c9c79f14dd0067e43bbcc4b9e90c3
Stan <kulyavtsev@yahoo.com>
2015-10-24 09:53:21 -0700

    add routes and common headers

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -3,22 +3,42 @@
 
   angular
     .module('travelNg')
-    .config(routeConfig);
+    .config(routeConfig)
+    .config(httpConfig)
+    .config(locationConfig);
 
   function routeConfig($routeProvider) {
     $routeProvider
-      .when('/', {
-        templateUrl: 'app/tour/index.html',
-        controller: 'TourController',
-        controllerAs: 'TourCtrl'
+      .when('/admin/tours', {
+        templateUrl: 'app/admin/tours/index.html',
+        controller: 'AdminToursController'
       })
-      .when('/countries', {
-        templateUrl: 'app/country/index.html',
-        controller: 'CountryController'
+      .when('/admin/countries', {
+        templateUrl: 'app/admin/countries/index.html',
+        controller: 'CountriesController'
+      })
+      .when('/tours', {
+        templateUrl: 'app/tours/index.html',
+        controller: 'ToursController'
+      })
+      .when('/tours/:slug', {
+        templateUrl: 'app/tours/show.html',
+        controller: 'TourController'
       })
       .otherwise({
-        redirectTo: '/'
+        redirectTo: '/tours'
       });
+  }
+
+  function httpConfig($httpProvider) {
+    $httpProvider.defaults.headers.common = {
+      'X-Parse-Application-Id': 'P3yrQQP1Ski2sA6eMuEcXLTugsntCXkDdhseyk13',
+      'X-Parse-REST-API-Key': 'MHnGP58WItHoXAJOpyiAh71gZQfJPEnabHcnbps5'
+    }
+  }
+
+  function locationConfig($locationProvider) {
+    $locationProvider.html5Mode(true);
   }
 
 })();
