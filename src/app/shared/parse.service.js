0928d8686fb3d19d6a4a1e4bb74090908ee5278d
Stan <kulyavtsev@yahoo.com>
2015-11-01 14:49:38 -0800

    move common code to parseCom

--- src/app/shared/parse.service.js
+++ src/app/shared/parse.service.js
@@ -11,6 +11,10 @@
       return data.results;
     };
 
+    var capitalize = function(name) {
+      return name.replace(/^./, name[0].toUpperCase());
+    };
+
     this.object = function(obj) {
       var url = 'https://api.parse.com/1/classes/' + obj + '/:objectId';
 
@@ -22,6 +26,18 @@
           update: { method: 'PUT' }
         }
       );
+    };
+
+    this.pointer = function(objectId, className) {
+      if (objectId) {
+        return {
+          "__type":"Pointer",
+          "className": capitalize(className),
+          "objectId": objectId
+        };
+      } else {
+        return {};
+      }
     };
   }
 })();
