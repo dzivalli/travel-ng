de40c9abc4c3e08366e451c80ee59107061932ed
Stan <kulyavtsev@yahoo.com>
2015-11-26 16:11:16 -0800

    rearrange directories, move folders to components dir

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -8,27 +8,27 @@
   function routeConfig($routeProvider) {
     $routeProvider
       .when('/admin/tours', {
-        templateUrl: 'app/admin/tours/index.html',
+        templateUrl: 'app/components/admin/tours/index.html',
         controller: 'AdminToursController'
       })
       .when('/admin/countries', {
-        templateUrl: 'app/admin/countries/index.html',
+        templateUrl: 'app/components/admin/countries/index.html',
         controller: 'CountriesController'
       })
       .when('/admin/places', {
-        templateUrl: 'app/admin/places/index.html',
+        templateUrl: 'app/components/admin/places/index.html',
         controller: 'PlacesController'
       })
       .when('/admin/hotels', {
-        templateUrl: 'app/admin/hotels/index.html',
+        templateUrl: 'app/components/admin/hotels/index.html',
         controller: 'HotelsController'
       })
       .when('/tours', {
-        templateUrl: 'app/tours/index.html',
+        templateUrl: 'app/components/tours/index.html',
         controller: 'ToursController'
       })
       .when('/tours/:slug', {
-        templateUrl: 'app/tours/show.html',
+        templateUrl: 'app/components/tours/show.html',
         controller: 'TourController'
       })
       .otherwise({
