af824d213aeb02b01f96133036e0f09fa16aaf7a
Stan <kulyavtsev@yahoo.com>
2015-12-18 09:32:30 -0800

    fix associated object is null or undefined

--- src/app/components/tours/tour.controller.js
+++ src/app/components/tours/tour.controller.js
@@ -7,8 +7,15 @@
 
   function TourController($scope, $routeParams, Tour, Country, Place, Hotel, parseCom) {
     $scope.tour = Tour.get({objectId: $routeParams.slug}, function(data) {
-      $scope.country = Country.get({objectId: data.country.objectId});
-      $scope.place = Place.get({objectId: data.place.objectId});
+      if (data.country && data.country.objectId) {
+        $scope.country = Country.get({objectId: data.country.objectId});
+      }
+
+      if (data.place && data.place.objectId) {
+        $scope.place = Place.get({objectId: data.place.objectId});
+
+      }
+
       Hotel.get(parseCom.objectByTour(data.objectId), function(data) {
         $scope.hotel = data.results[0];
       });
